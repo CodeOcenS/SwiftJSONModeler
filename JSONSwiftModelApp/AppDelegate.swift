5eb1df20ca008856b0710840c75032ea36201426
Sven <liwanfu5@hikcreate.com>
2020-01-22 11:32:05 +0800

    Initial Commit

---
+++ JSONSwiftModelApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSONSwiftModelApp
+//
+//  Created by Sven on 2020/1/22.
+//  Copyright © 2020 Sven. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSONSwiftModelApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONSwiftModelApp
+//
+//  Created by Sven on 2020/1/22.
+//  Copyright © 2020 Sven. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
