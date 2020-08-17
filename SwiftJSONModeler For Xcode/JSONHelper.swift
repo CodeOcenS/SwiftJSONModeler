4a1bcac56c1233acd396f2645920199bc90e5bb1
Sven <liwanfu5@hikcreate.com>
2020-08-17 15:09:12 +0800

    feat: json 转模 支持多层json 嵌套。

---
+++ SwiftJSONModeler For Xcode/JSONHelper.swift
@@ -0,0 +1,93 @@
+//
+//  JSONHelper.swift
+//  SwiftJSONModeler For Xcode
+//
+//  Created by Sven on 2020/8/17.
+//  Copyright © 2020 Sven. All rights reserved.
+//
+/// 将 josn 转化为 YapiObject
+
+import Foundation
+
+public struct JSONHelper {
+    /// 复制的文本
+    var paste: String
+    /// 将复制的文本 转化为 YApiObject 对象。
+    func transform() -> YApiObject? {
+        do {
+            let dicJson = try JSONSerialization.jsonObject(with: paste.data(using: .utf8)!, options: .mutableLeaves)
+            if let dic = dicJson as? [String: Any]{
+                return objectOf(dic, key: "")
+            } else if let arr = dicJson as? [Any] {
+                return objectOf(arr, key: "")
+            } else {
+                return nil
+            }
+        } catch let e {
+            print(e)
+            ErrorCenter.shared.message = "json 序列化失败，请检查 json 数据"
+            return nil
+    
+        }
+    }
+    
+    /// 字典转对象
+    /// - Parameter dic: 字典数据
+    private func objectOf(_ dic: [String: Any], key: String) -> YApiObject {
+        var childs: [YApiObject] = []
+        for (label, value) in dic {
+            let child = typeOf(label, value: value, parentKey: key)
+            childs.append(child)
+        }
+         let object = YApiObject(parentKey: nil, key: key, mock: "", type: YApiType.object, typeRaw: "Object", des: "", childs: childs)
+        return object
+    }
+    
+    /// 数组转对象
+    /// - Parameter arry: 数据数组
+    private func objectOf(_ arry: [Any], key:String) -> YApiObject {
+        let type = YApiType.array
+        guard  let first = arry.first  else {
+            ErrorCenter.shared.message = "json 序列化 数组第一个元素类型判断异常"
+            let childType = YApiType.undefined
+            let objct = YApiObject(parentKey: key, key: key, mock: "", type: childType, typeRaw: "<#Undefined#>", des: nil, childs: [])
+            let arrObjct = YApiObject(parentKey: key, key: key, mock: "", type: type, typeRaw: "Array", des: nil, childs: [objct])
+            return arrObjct
+        }
+        let arrObjct = YApiObject(parentKey: nil, key: key, mock: "", type: type, typeRaw: "Array", des: nil, childs: [typeOf(nil, value: first)])
+        return arrObjct
+    }
+    /// 类型数据类型判断
+    /// - Parameter value: 值
+    private func typeOf(_ key: String?, value: Any, parentKey: String? = nil) -> YApiObject {
+        if value is NSNull {
+            print("存在null")
+            let type = YApiType.undefined
+            let objct = YApiObject(parentKey: parentKey, key: key, mock: "", type: type, typeRaw: "<#NSNull#>", des: nil, childs: [])
+            return objct
+        } else if value is String {
+            let type = YApiType.string
+            let objct = YApiObject(parentKey: parentKey, key: key, mock: "", type: type, typeRaw: "String", des: nil, childs: [])
+            return objct
+        } else if value is Int {
+            let type = YApiType.integer
+            let objct = YApiObject(parentKey: parentKey, key: key, mock: "", type: type, typeRaw: "Int", des: nil, childs: [])
+            return objct
+        } else if value is Double {
+            let type = YApiType.number
+            let objct = YApiObject(parentKey: parentKey, key: key, mock: "", type: type, typeRaw: "Double", des: nil, childs: [])
+            return objct
+        } else if  value is Bool {
+            let type = YApiType.boolean
+            let objct = YApiObject(parentKey: parentKey, key: key, mock: "", type: type, typeRaw: "Bool", des: nil, childs: [])
+            return objct
+        } else if let arr =  value as? [Any] {
+            return objectOf(arr, key: key ?? "")
+        } else if let dic = value as? [String: Any]{
+            return objectOf(dic, key: key ?? "")
+        } else {
+            ErrorCenter.shared.message = "json 格式异常，无法解析为 model"
+            return YApiObject()
+        }
+    }
+}
---
+++ SwiftJSONModeler For XcodeTests/SwiftJSONModelerTest/JSONHelperTest.swift
@@ -0,0 +1,148 @@
+//
+//  JSONHelperTest.swift
+//  SwiftJSONModeler For XcodeTests
+//
+//  Created by Sven on 2020/8/17.
+//  Copyright © 2020 Sven. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftJSONModeler_For_Xcode
+
+class JSONHelperTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    /// 测试异常 json 格式
+    func testAbnormalJSON() -> Void {
+        let json = """
+        {
+            "title": "这不是一个正确的 json",
+            "property1": "第一个属性" // 不加逗号
+            property: "" // 没用引号
+        }
+        """
+        let helper = JSONHelper(paste: json)
+        let object = helper.transform()
+        XCTAssertNil(object)
+    }
+    
+    /// 测试 第一层为数组 json
+    func testArrayJSON() {
+        let json =
+        """
+        [
+            "value1",
+            "value2",
+            "value3"
+        ]
+        """
+        let helper = JSONHelper(paste: json)
+        let object = helper.transform()
+        XCTAssertNotNil(object)
+        XCTAssert(object!.childs.first!.type == YApiType.string, "数组类型应该为 String")
+    }
+    
+    /// 测试 数组为空情况
+    func testEmptyArray() {
+        let json =
+        """
+        {
+            "title": "空数组",
+            "emptyArray": []
+        }
+        """
+        let helper = JSONHelper(paste: json)
+        let object = helper.transform()
+        XCTAssertNotNil(object)
+        XCTAssert(object!.childs.filter { $0.key == "emptyArray" }.first!.childs.first!.type == YApiType.undefined, "数组类型应该为 Undefined")
+    }
+    
+    /// 测试 单层 接送
+    func testSingleJSON() -> Void {
+        let json =
+        """
+        {
+        "title": "第一层 json",
+        "stringValue": "字符串值",
+        "intValue": 58,
+        "doubleValue": 18.2,
+        "nullValue": null,
+        "boolValue": true
+        }
+        """
+        let helper = JSONHelper(paste: json)
+        let object = helper.transform()
+        XCTAssertNotNil(object)
+        let objc = object!
+       
+        XCTAssertNotNil(objc.childs.filter { $0.key == "stringValue" }.first)
+        XCTAssertNotNil(objc.childs.filter { $0.key == "intValue" }.first)
+        XCTAssertNotNil(objc.childs.filter { $0.key == "boolValue" }.first)
+        XCTAssertNotNil(objc.childs.filter { $0.key == "nullValue" }.first)
+        XCTAssertNotNil(objc.childs.filter { $0.key == "doubleValue" }.first)
+
+    }
+    
+    /// 测试多层嵌套 json
+    func testMultiLevelJSON() -> Void {
+        let json =
+        """
+        {
+            "title": "第一层 json",
+            "stringValue": "字符串值",
+            "intValue": 58,
+            "doubleValue": 18.2,
+            "nullValue": null,
+            "boolValue": true,
+            "subJson": {
+                "title": "第二层 json",
+                "stringValue": "字符串值"
+            },
+            "arrayValue1": [
+                "value1",
+                "value2",
+                "value3"
+            ],
+            "arrayValue2": [{
+                "title": "数组包含子 json",
+                "intValue": 12,
+                "boolValue": false
+            }]
+        }
+        """
+        let helper = JSONHelper(paste: json)
+        let object = helper.transform()
+        XCTAssertNotNil(object)
+        guard object != nil else {
+            return
+        }
+        let subJsonArr = object!.childs.filter{ $0.key == "subJson" }
+        XCTAssertNotNil(subJsonArr.first)
+        guard let subJson = subJsonArr.first else {
+            return
+        }
+        XCTAssert(subJson.childs.count == 2, "subJson 属性数量为2")
+        XCTAssert(object!.childs.filter { $0.key == "arrayValue1" }.first!.childs.first!.type == YApiType.string, "arrayValue1 数组类型应该为 String")
+        XCTAssert(object!.childs.filter { $0.key == "arrayValue2" }.first!.childs.first!.type == YApiType.object, "arrayValue2 数组类型应该为 object")
+    }
+
+}
